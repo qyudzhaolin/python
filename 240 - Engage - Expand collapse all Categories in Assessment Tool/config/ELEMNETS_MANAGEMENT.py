24b22e35b91afcf7727289544a01363893ea708b
zhaoxin <977145983@qq.com>
2019-12-26 11:24:12 +0800

    pycharm git

---
+++ 240 - Engage - Expand collapse all Categories in Assessment Tool/config/ELEMNETS_MANAGEMENT.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+#jump url
+URL_MANAGEMENT = [By.XPATH, '//*[@class=\'parent-dashboard-content-admini-list\']/div[1]/a/p']
+#menu
+CLASS_ROSTER_MANAGEMENT = [By.XPATH, '//*[@class=\'main-con-left menu-side\']/ul/li[11]/a']
+#Community
+COMMUNITY = [By.ID, 'txtCommunity']
+#School Name
+SCHOOL_NAME = [By.ID, 'txtSchool']
